1ba9bd14920d46fbb2512c8d73e4339958fcc528
SerikRakhimov <s_astana@mail.ru>
2020-12-19 01:51:18 +0600

    New tables added

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-6 text-left align-top">
-                <h3>{{trans('main.templates')}}</h3>&nbsp;
+                <h3>{{trans('main.templates')}}&nbsp;</h3>
             </div>
 {{--            <div class="col-1">--}}
 {{--            </div>--}}
