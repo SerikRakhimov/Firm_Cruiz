7e85a256dc65592eb42b0757e1063afc544f0a99
SerikRakhimov <s_astana@mail.ru>
2020-12-19 01:38:50 +0600

    New tables added

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -4,14 +4,17 @@
     <p>
     <div class="container-fluid">
         <div class="row">
-            <div class="col text-left align-top">
+            <div class="col-6 text-left align-top">
                 <h3>{{trans('main.templates')}}</h3>
             </div>
-            <div class="col-6 text-right">
-{{--                <a href="{{route('template.create')}}" title="{{trans('main.add')}}">--}}
-{{--                    <img src="{{Storage::url('add_record.png')}}" width="15" height="15" alt="{{trans('main.add')}}">--}}
-{{--                </a>--}}
-                <button type="button" class="btn btn-purple" title="{{trans('main.add')}}" onclick="document.location='{{route('template.create')}}'">
+            <div class="col-1">
+            </div>
+            <div class="col-5 text-right">
+                {{--                <a href="{{route('template.create')}}" title="{{trans('main.add')}}">--}}
+                {{--                    <img src="{{Storage::url('add_record.png')}}" width="15" height="15" alt="{{trans('main.add')}}">--}}
+                {{--                </a>--}}
+                <button type="button" class="btn btn-purple" title="{{trans('main.add')}}"
+                        onclick="document.location='{{route('template.create')}}'">
                     <i class="fa fa-plus fa-fw"></i> {{trans('main.add')}}
                 </button>
             </div>
