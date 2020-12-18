43db363315765e25dd12e0bb8193795e1a661e51
SerikRakhimov <s_astana@mail.ru>
2020-12-19 01:49:03 +0600

    New tables added

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -4,11 +4,11 @@
     <p>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-5 text-left align-top">
-                <h3>{{trans('main.templates')}}</h3>
+            <div class="col-6 text-left align-top">
+                <h3>{{trans('main.templates')}} </h3>
             </div>
-            <div class="col-1">
-            </div>
+{{--            <div class="col-1">--}}
+{{--            </div>--}}
             <div class="col-6 text-right">
                 {{--                <a href="{{route('template.create')}}" title="{{trans('main.add')}}">--}}
                 {{--                    <img src="{{Storage::url('add_record.png')}}" width="15" height="15" alt="{{trans('main.add')}}">--}}
