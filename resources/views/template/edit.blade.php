fe72290e13f695401288427a4fbbd8f831beef97
SerikRakhimov <s_astana@mail.ru>
2020-12-20 00:50:27 +0600

    Added

--- resources/views/template/edit.blade.php
+++ resources/views/template/edit.blade.php
@@ -71,7 +71,7 @@
         </div>
 
         <div class="row text-center no-gutters">
-            <div class="col-6">
+            <div class="col">
                 <button type="submit" class="btn btn-dreamer"
                         @if (!$update)
                         title="{{trans('main.add')}}">
@@ -82,7 +82,7 @@
                     @endif
                 </button>
             </div>
-            <div class="col-6">
+            <div class="col">
                 <button type="button" class="btn btn-dreamer" title="{{trans('main.cancel')}}"
                     @include('layouts.previous_url')
                 >
