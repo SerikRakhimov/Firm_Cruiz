dcbcd8db5b9b26cb686b2441beeed9d5adef1a33
SerikRakhimov <s_astana@mail.ru>
2020-12-20 01:18:24 +0600

    Added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -124,7 +124,7 @@
         </div>
     </nav>
 
-    <main class="py-4 w-95 mx-auto">
+    <main class="py-4 w-75 mx-auto">
 {{--        <div class="mx-auto" style="width: 1200px;">--}}
         @yield('content')
 {{--        </div>--}}
