31e0a575899c3ffbea81384217e4b2b9a77b7e52
SerikRakhimov <s_astana@mail.ru>
2021-01-07 01:51:03 +0600

    Added

--- resources/views/layouts/template/show_name.blade.php
+++ resources/views/layouts/template/show_name.blade.php
@@ -1,7 +1,11 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12 text-center">
-            <h4><a href="{{route('template.index')}}" title="{{trans('main.template')}}" class="text-warning">{{trans('main.template')}}</a><span class="text-warning">:</span>
+            <h4>    @if ( Auth::user()->isAdmin()))
+                <a href="{{route('template.index')}}" title="{{trans('main.template')}}" class="text-warning">{{trans('main.template')}}:</a>
+                @else
+                    {{trans('main.template')}}:
+                @endif
                 <a href={{route('template.show', $template)}}title="{{$template->name()}}">{{$template->name()}}</a>
             </h4>
         </div>
