bd7bae2d98b7f37fb80f9fd781c2e02064ab5f1d
SerikRakhimov <s_astana@mail.ru>
2021-01-11 12:52:14 +0600

    Added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,11 +41,11 @@ use App\Models\Project;
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
             </a>
-            {{--            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"--}}
-            {{--                    aria-controls="navbarSupportedContent" aria-expanded="false"--}}
-            {{--                    aria-label="{{ __('Toggle navigation') }}">--}}
-            {{--                <span class="navbar-toggler-icon"></span>--}}
-            {{--            </button>--}}
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             @auth
                 <!-- Left Side Of Navbar -->
