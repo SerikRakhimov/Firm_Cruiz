857f4e7abb29b864fb06247449d9d468d85727b5
SerikRakhimov <s_astana@mail.ru>
2021-02-19 20:13:46 +0600

    Added

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::get('/', function () {
     }
 
     GlobalController::glo_project_role_setnull();
-    	Mail::send(['html'=>'mail_run'], ['remote_addr'=>$_SERVER['REMOTE_ADDR'], 'http_user_agent'=>$_SERVER['HTTP_USER_AGENT']], function($message){
+    	Mail::send(['html'=>'mail\mail_run'], ['remote_addr'=>$_SERVER['REMOTE_ADDR'], 'http_user_agent'=>$_SERVER['HTTP_USER_AGENT']], function($message){
     		$message->to('s_astana@mail.ru','')->subject('Игра запущена');
     		$message->from('admin@guess.rsb0807.kz','Игра Задумай-угадаю');
     	});
