a08e86d8578d09af0f2a05d63b9a3de24d298baf
SerikRakhimov <s_astana@mail.ru>
2021-03-03 23:17:47 +0600

    Added

--- resources/views/layouts/show_project_role.blade.php
+++ resources/views/layouts/show_project_role.blade.php
@@ -4,7 +4,7 @@
         <div class="col-3 text-left">
             <a href="{{route('base.template_index', ['project' => $project, 'role' => $role])}}" title="{{trans('main.bases')}}">
 {{--                <span class="badge badge-success">{{$project->name()}}</span>--}}
-                <span class="badge badge-success">{{$project->user->name()}}</span>
+                <span class="badge badge-success">{{Auth::user()->name()}}</span>
             </a>
         </div>
         <div class="col-6 text-center">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,8 +100,8 @@
 {{--                    Abakus - учетная платформа--}}
 {{--                </div>--}}
 
-{{--                <div class="links">--}}
-{{--                    <a href="https://laravel.com/docs">Docs</a>--}}
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
 {{--                    <a href="https://laracasts.com">Laracasts</a>--}}
 {{--                    <a href="https://laravel-news.com">News</a>--}}
 {{--                    <a href="https://blog.laravel.com">Blog</a>--}}
@@ -109,7 +109,7 @@
 {{--                    <a href="https://forge.laravel.com">Forge</a>--}}
 {{--                    <a href="https://vapor.laravel.com">Vapor</a>--}}
 {{--                    <a href="https://github.com/laravel/laravel">GitHub</a>--}}
-{{--                </div>--}}
+                </div>
             </div>
         </div>
     </body>
