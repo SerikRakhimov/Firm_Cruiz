383e81dd0f0d606036d8b06476bf22c20f080066
SerikRakhimov <s_astana@mail.ru>
2021-04-18 16:54:26 +0600

    Added

--- resources/views/mail/item_create.blade.php
+++ resources/views/mail/item_create.blade.php
@@ -1,5 +1,8 @@
 <?php
 use App\Http\Controllers\GlobalController;
+$tile_view = $item->base->tile_view($base_right);
+$link_image = $tile_view['link'];
+$item_find = MainController::view_info($item->id, $link_image->id);
 ?>
 <p>{{trans('main.project')}}: <b>{{$item->project->name()}}</b></p>
 <hr>
@@ -8,6 +11,9 @@ use App\Http\Controllers\GlobalController;
 <p>Id: <b>{{$item->id}}</b></p>
 @if($item->base->is_code_needed == true)
     <p>{{trans('main.code')}}: <b>{{$item->code}}</b></p>
+@endif
+@if($item_find)
+    @include('view.img',['item'=>$item_find, 'size'=>"medium", 'filenametrue'=>false, 'link'=>false, 'img_fluid'=>true, 'title'=>$item->name()])
 @endif
 <p>{{trans('main.name')}}:<br><b><?php echo $item->nmbr();?></b></p><br>
 <p class="text-label">{{trans('main.created_user_date_time')}}:
