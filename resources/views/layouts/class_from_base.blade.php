9ddc570364267784d43cc2daf8553ca26d292819
SerikRakhimov <s_astana@mail.ru>
2021-04-24 02:45:29 +0600

    Added

--- resources/views/layouts/class_from_base.blade.php
+++ resources/views/layouts/class_from_base.blade.php
@@ -9,14 +9,14 @@ $is_align_middle = isset($align_middle);
 @elseif($base->type_is_date() || $base->type_is_boolean() || $base->type_is_image() || $base->type_is_document())
     {{--    Использовать именно так, как в строках ниже--}}
     @if($is_align_middle)
-        class="text-center align_middle"
+        class="text-center align-middle"
     @else
         class="text-center"
     @endif
 @else
     {{--    Использовать именно так, как в строках ниже--}}
     @if($is_align_middle)
-        class="text-left align_middle"
+        class="text-left align-middle"
     @else
         class="text-left"
     @endif
