fd2f95e6b7743b45841ba6a9a7fffd3070ec4777
SerikRakhimov <s_astana@mail.ru>
2021-04-24 02:37:17 +0600

    Added

--- resources/views/layouts/class_from_base.blade.php
+++ resources/views/layouts/class_from_base.blade.php
@@ -1,10 +1,21 @@
+<?php
+$is_align_middle = isset($align_middle);
+?>
 {{--                                если тип корректировки поля - число--}}
 @if($base->type_is_number())
     class="text-right"
     {{--                                если тип корректировки поля - дата--}}
     {{--                                если тип корректировки поля - логический--}}
 @elseif($base->type_is_date() || $base->type_is_boolean() || $base->type_is_image() || $base->type_is_document())
-    class="text-center"
+    class="text-center
+    @if($is_align_middle)
+        align_middle
+    @endif
+    "
 @else
-    class="text-left"
+    class="text-left
+    @if($is_align_middle)
+        align_middle
+    @endif
+    "
 @endif
