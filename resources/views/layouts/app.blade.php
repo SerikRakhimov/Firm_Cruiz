88d3f34de46a446483b16df978614a1448c071d9
SerikRakhimov <s_astana@mail.ru>
2021-04-26 01:34:01 +0600

    Added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -206,9 +206,7 @@ use App\Models\Project;
         @guest
 {{--            Похожие строки layouts\app.blade.php и message.blade.php--}}
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <p>
                 <h5 class="display-5 text-danger text-center">{{trans('main.please_login_or_register')}}</h5>
-                </p>
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
