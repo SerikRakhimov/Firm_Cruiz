a261676f1c92a3bef6b134a6c5ac06179b7f5d46
SerikRakhimov <s_astana@mail.ru>
2021-06-18 01:26:58 +0600

    Added

--- resources/views/item/base_index.blade.php
+++ resources/views/item/base_index.blade.php
@@ -191,6 +191,9 @@
                                 <?php echo $item->nmbr(false);?>
                             </a></h5>
                         {{--                    </div>--}}
+                        <div class="card-footer">
+                            {{$this->created_at->Format(trans('main.format_date_time'))}}
+                        </div>
                     </div>
                 </div>
             @endforeach
