06627b874870475b4486d3a5c2be55b6625e5ede
SerikRakhimov <s_astana@mail.ru>
2021-07-01 21:34:46 +0600

    Added

--- resources/views/mail/access_create.blade.php
+++ resources/views/mail/access_create.blade.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\GlobalController;
 use App\Http\Controllers\MainController;
+use App\Http\ControllersProjectController;
 $project = $access->project;
 $role = $access->role;
 $user= GlobalController::glo_user();
