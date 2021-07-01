e116550cdffd18ab6b02752275906095d2ac7807
SerikRakhimov <s_astana@mail.ru>
2021-07-01 21:36:22 +0600

    Added

--- resources/views/mail/access_delete.blade.php
+++ resources/views/mail/access_delete.blade.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\GlobalController;
 use App\Http\Controllers\MainController;
-use App\Http\ControllersProjectController;
+use App\Http\Controllers\ProjectController;
 $project = $access->project;
 $role = $access->role;
 $user= GlobalController::glo_user();
