28fd8eaf69bf58511383de9cc20bd41edac36831
SerikRakhimov <s_astana@mail.ru>
2021-09-29 18:48:32 +0600

    Added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,4 +43,9 @@ class LoginController extends Controller
 //        $this->middleware('guest', ['except' => ['logout', 'getLogout']]);
     }
 
+// https://ru.stackoverflow.com/questions/622264/laravel-%D1%80%D0%B5%D0%B4%D0%B8%D1%80%D0%B5%D0%BA%D1%82-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5-%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8
+    protected function redirectTo(){
+        return url('/home');
+    }
+
 }
